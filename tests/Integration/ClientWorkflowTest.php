d1c5520481f1ad5edca84d6f1ed9081282973727
calliostro <calliostro@gmail.com>
2025-10-26 14:10:06 +0100

    fix: PHP 8.5 compatibility - use grouped CoversClass attributes

--- tests/Integration/ClientWorkflowTest.php
+++ tests/Integration/ClientWorkflowTest.php
@@ -19,8 +19,10 @@ use RuntimeException;
 /**
  * Integration tests for the complete client workflow
  */
-#[CoversClass(DiscogsClientFactory::class)]
-#[CoversClass(DiscogsClient::class)]
+#[
+    CoversClass(DiscogsClientFactory::class),
+    CoversClass(DiscogsClient::class)
+]
 final class ClientWorkflowTest extends IntegrationTestCase
 {
     /**
